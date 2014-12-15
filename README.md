4b0da919a773783df738eb9038218ae349581f37
Sebastián Grignoli <grignoli@gmail.com>
2014-12-15 16:15:37 -0300

    Initial import

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-sharethislink.js
+ShareThisLink.js
 ================
 
-Tiny script to implement share buttons on web pages.
+Usage:
+------
+
+    <script type="text/javascript" src="/js/ShareThisLink.js"></script>
+
+    <script type="text/javascript">
+        var share = new shareThisLink(
+        {
+          'url':   'http://server.com/article/url/1314',
+          'title': 'Article title here',
+          'summary': 'Article summary here',
+          'image': 'http://server.com/pics/image.jpg',
+          'via': '@TwitterUser'
+        });
+    </script>
+
+    <div>
+      <a onclick="share.Facebook();">Share in Facebook</a>
+      <a onclick="share.Twitter();">Share in Twitter</a>
+      <a onclick="share.LinkedIn();">Share in Linkedin</a>
+      <a onclick="share.Mail();">Share by mail</a>
+    </div>
+
+
+Facebook, Twitter and Linkedin just work out of the box.
+Mail() simply opens a popup to your site's "/share/mail" url, so you have to implement that part for yourself.
---
+++ ShareThisLink.js
@@ -0,0 +1,63 @@
+function shareThisLink(data){
+  this.shareData = data;
+  this.shareData['title'] = encodeURIComponent(this.shareData['title']);
+  this.shareData['summary'] = encodeURIComponent(this.shareData['summary']);
+  
+  var initialized = false;
+
+  this.initialize = function(){
+    if(initialized) return; 
+    initialized = true; 
+    if(this.shareData['url']) this.shareData['shortUrl'] = this.getShortUrl(this.shareData['url']);
+  }
+
+  this.Facebook = function(){
+    this.initialize();
+    var data = ___merge(this.shareData, this.shareData['facebook']);
+    window.open( 
+        "http://www.facebook.com/sharer/sharer.php?u="+this.shareData['shortUrl']+"&t="+this.shareData['title']+"&images[0]="+this.shareData['image'], 
+        this.target, 'width=500,height=400');
+  }
+ 
+  this.Twitter = function(){
+    this.initialize();
+    var data = ___merge(this.shareData, this.shareData['twitter']);
+    window.open(
+        "https://twitter.com/intent/tweet?text="+data['title']+" "+data['shortUrl']+(data["via"]?" via "+data["via"]:""),
+        //"http://twitter.com/share?text="+this.shareData['title']+" "+this.shareData['shortUrl'],
+        this.target, 'width=500,height=400');
+  }
+
+  this.LinkedIn = function(){
+    this.initialize();
+    var data = ___merge(this.shareData, this.shareData['linkedin']);
+    window.open( 
+        "http://www.linkedin.com/shareArticle?mini=true&url="+this.shareData['shortUrl']+"&title="+this.shareData['title']+"&summary="+this.shareData['summary'],
+        this.target, 'width=500,height=400');
+  }
+
+  this.Mail = function(){
+    this.initialize();
+    var data = ___merge(this.shareData, this.shareData['mail']);
+    window.open( 
+        "/share/mail?url="+this.shareData['url']+"&short_url="+this.shareData['shortUrl']+"&title="+this.shareData['title']+"&summary="+this.shareData['summary']+"&image="+this.shareData['image'],
+        this.target, 'width=600,height=500');
+  }
+
+  this.getShortUrl = function(url){
+    var xhReq = new XMLHttpRequest();
+    xhReq.open("GET", "/share/shorten?url="+encodeURIComponent(url), false);
+    xhReq.send(null);
+    return xhReq.responseText;
+  }
+
+  return this; 
+}
+
+function ___merge(set1, set2){
+  for (var key in set2){
+    if (set2.hasOwnProperty(key))
+      set1[key] = set2[key]
+  }
+  return set1
+}
